37864aeb590b55ddd1b74538d9cec7e2b53876a1
Muhammad Shafrizal <muhammadshafrizal@Muhammads-MacBook-Air.local>
2025-08-14 15:20:44 +0700

    compare fetch ip between server and client component

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,41 +1,42 @@
-'use server';
+"use server";
 
-import { StayDate } from '@/components/ui/stay-date';
-import RoomGuest from '@/components/ui/room-guest';
-import Hotel from '@/components/ui/hotel';
-import '@/app/globals.css';
-import SearchRoomButton from '@/components/ui/search-room-button';
+import { StayDate } from "@/components/ui/stay-date";
+import RoomGuest from "@/components/ui/room-guest";
+import Hotel from "@/components/ui/hotel";
+import "@/app/globals.css";
+import SearchRoomButton from "@/components/ui/search-room-button";
 export default async function SearchPage({
-    searchParams,
+  searchParams,
 }: Readonly<{
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }>) {
-    const search = await searchParams;
-    return (
-        <main className="w-full bg-neutral-100 min-h-screen">
-            <div className="bg-white py-2 md:py-4 shadow-[0_6px_8px_rgba(0,0,0,0.08)] sticky top-0 z-10">
-                <div className="container mx-auto flex flex-col gap-4 items-center justify-center h-full px-6 2xl:px-0">
-                    <div
-                        id="search-filter"
-                        className="container mx-auto 2xl:px-0"
-                    >
-                        <div
-                            className={
-                                'bg-white flex flex-col md:flex-row gap-4 rounded-md border border-neutral-200 p-3 md:p-6'
-                            }
-                        >
-                            <StayDate isAuthenticated={true} />
-                            <RoomGuest type="TYPE_2" />
-                            <SearchRoomButton />
-                        </div>
-                    </div>
-                </div>
+  const search = await searchParams;
+  const ipData = await fetch("https://ipapi.co/json/");
+  const ipDataJson = await ipData.json();
+  console.log(ipDataJson);
+  return (
+    <main className="w-full bg-neutral-100 min-h-screen">
+      <div className="bg-white py-2 md:py-4 shadow-[0_6px_8px_rgba(0,0,0,0.08)] sticky top-0 z-10">
+        <div className="container mx-auto flex flex-col gap-4 items-center justify-center h-full px-6 2xl:px-0">
+          <div id="search-filter" className="container mx-auto 2xl:px-0">
+            <div
+              className={
+                "bg-white flex flex-col md:flex-row gap-4 rounded-md border border-neutral-200 p-3 md:p-6"
+              }
+            >
+              <StayDate isAuthenticated={true} />
+              <RoomGuest type="TYPE_2" />
+              <SearchRoomButton />
             </div>
-            <div className="container px-6 2xl:px-0 mx-auto flex flex-col gap-4 pt-6 pb-[100px]">
-                <Hotel property_name="wMvbmOeYAl" searchParams={search} />
-                <Hotel property_name="wMvbmOeYAl" searchParams={search} />
-                <Hotel property_name="wMvbmOeYAl" searchParams={search} />
-            </div>
-        </main>
-    );
+          </div>
+        </div>
+      </div>
+      <pre className="text-xs">{JSON.stringify(ipDataJson, null, 2)}</pre>
+      <div className="container px-6 2xl:px-0 mx-auto flex flex-col gap-4 pt-6 pb-[100px]">
+        <Hotel property_name="wMvbmOeYAl" searchParams={search} />
+        <Hotel property_name="wMvbmOeYAl" searchParams={search} />
+        <Hotel property_name="wMvbmOeYAl" searchParams={search} />
+      </div>
+    </main>
+  );
 }
--- components/ui/home-award.tsx
+++ components/ui/home-award.tsx
@@ -1,74 +1,85 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from '@/components/ui/carousel';
-import { useTranslations } from 'next-intl';
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
 
 export default function HomeAward() {
-    const t = useTranslations('Home');
-    const awards = [
-        {
-            img_url: '/images/award-1.png',
-            title: 'Traveler Choice Award, Rank #18  Top 25 Luxury Hotels - Asia by Tripadvisor in 2024',
-        },
-        {
-            img_url: '/images/award-2.png',
-            title: 'Traveler Choice Award, Rank #18  Top 25 Luxury Hotels - Asia by Tripadvisor in 2023',
-        },
-        {
-            img_url: '/images/award-3.png',
-            title: 'Rank #2 Family Resort in Bali by Holidays with Kids in 2023',
-        },
-    ];
-    return (
-        <section id="award" className="bg-neutral-200 py-10 mt-10">
-            <div className="flex flex-col gap-4 container px-6 sm:px-0 mx-auto xl:max-w-[1040px]">
-                <h2 className={'h2 uppercase'}>{t('award')}</h2>
-                <Carousel
-                    className="w-full"
-                    id="home-award-carousel"
-                    data-testid="home-award-carousel"
-                    opts={{
-                        align: 'start',
-                        loop: true,
-                    }}
+  const t = useTranslations("Home");
+  const [clientIp, setClient] = useState();
+  const awards = [
+    {
+      img_url: "/images/award-1.png",
+      title:
+        "Traveler Choice Award, Rank #18  Top 25 Luxury Hotels - Asia by Tripadvisor in 2024",
+    },
+    {
+      img_url: "/images/award-2.png",
+      title:
+        "Traveler Choice Award, Rank #18  Top 25 Luxury Hotels - Asia by Tripadvisor in 2023",
+    },
+    {
+      img_url: "/images/award-3.png",
+      title: "Rank #2 Family Resort in Bali by Holidays with Kids in 2023",
+    },
+  ];
+
+  useEffect(() => {
+    // GET https://ipapi.co/{format}/
+    fetch("https://ipapi.co/json/")
+      .then((response) => response.json())
+      .then((data) => setClient(data))
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <section id="award" className="bg-neutral-200 py-10 mt-10">
+      <div className="flex flex-col gap-4 container px-6 sm:px-0 mx-auto xl:max-w-[1040px]">
+        <h2 className={"h2 uppercase"}>{t("award")}</h2>
+        <pre className="text-xs">{JSON.stringify(clientIp, null, 2)}</pre>
+        <Carousel
+          className="w-full"
+          id="home-award-carousel"
+          data-testid="home-award-carousel"
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+        >
+          <CarouselContent className="gap-4 px-4">
+            {awards.map((award, i) => {
+              return (
+                <CarouselItem
+                  key={i}
+                  className="p-6 flex gap-2 items-center bg-white border border-neutral-200 rounded-md basis-full md:basis-1/2 lg:basis-[336px]"
                 >
-                    <CarouselContent className="gap-4 px-4">
-                        {awards.map((award, i) => {
-                            return (
-                                <CarouselItem
-                                    key={i}
-                                    className="p-6 flex gap-2 items-center bg-white border border-neutral-200 rounded-md basis-full md:basis-1/2 lg:basis-[336px]"
-                                >
-                                    <Image
-                                        src={award.img_url}
-                                        alt={award.title}
-                                        height={56}
-                                        width={56}
-                                    />
-                                    <p className="text-neutral-600">
-                                        {award.title}
-                                    </p>
-                                </CarouselItem>
-                            );
-                        })}
-                    </CarouselContent>
-                    <CarouselPrevious
-                        className="-left-4 shadow-xl bg-white hover:bg-yellow-00 disabled:bg-white text-neutral-900 disabled:text-neutral-900 disabled:cursor-not-allowed! border-0 h-11 w-11"
-                        iconClassName="size-6 text-neutral-900"
-                    />
-                    <CarouselNext
-                        className="-right-4 shadow-xl bg-white hover:bg-yellow-00 disabled:bg-white text-neutral-900 disabled:text-neutral-900 disabled:cursor-not-allowed! border-0 h-11 w-11"
-                        iconClassName="size-6 text-neutral-900"
-                    />
-                </Carousel>
-            </div>
-        </section>
-    );
+                  <Image
+                    src={award.img_url}
+                    alt={award.title}
+                    height={56}
+                    width={56}
+                  />
+                  <p className="text-neutral-600">{award.title}</p>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          <CarouselPrevious
+            className="-left-4 shadow-xl bg-white hover:bg-yellow-00 disabled:bg-white text-neutral-900 disabled:text-neutral-900 disabled:cursor-not-allowed! border-0 h-11 w-11"
+            iconClassName="size-6 text-neutral-900"
+          />
+          <CarouselNext
+            className="-right-4 shadow-xl bg-white hover:bg-yellow-00 disabled:bg-white text-neutral-900 disabled:text-neutral-900 disabled:cursor-not-allowed! border-0 h-11 w-11"
+            iconClassName="size-6 text-neutral-900"
+          />
+        </Carousel>
+      </div>
+    </section>
+  );
 }
